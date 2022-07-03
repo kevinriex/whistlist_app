e09ede53fd4ba982d696e781805288cbf4e443d5
Kevin Riexinger <info@kevinriexinger.de>
2022-07-03 19:38:52 +0200

    Adding Description to Lists

---
+++ db/migrate/20220703173540_addding_desc_to_lists.rb
@@ -0,0 +1,6 @@
+class AdddingDescToLists < ActiveRecord::Migration[7.0]
+  def change
+    add_column :wish_lists, :description, :text
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_03_171216) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_03_173540) do
   create_table "wish_items", force: :cascade do |t|
     t.text "description"
     t.string "link"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_03_171216) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   add_foreign_key "wish_items", "wish_lists"
